ae1e5ed0a623a299042ad94f8248637beb216edb
bluekigh <bluekigh@gmail.com>
2023-07-22 21:20:26 +0900

    03까지

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private SpriteRenderer sprite;
+    private Rigidbody2D rigid2d;
+    private Animator anim;
+
+    public Vector2 inputvector;    
+
+    public float speed;
+    
+    private void Awake()
     {
-        
+        rigid2d = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue value)
     {
-        
+        inputvector = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        // // add force
+        // rigid2d.AddForce (inputvector);
+        //
+        // // velocity
+        // rigid2d.velocity = inputvector;
+
+        Vector2 nextVec = Time.fixedDeltaTime * inputvector * speed ;
+        // move position
+        rigid2d.MovePosition(rigid2d.position + inputvector);
+    }
+
+    private void LateUpdate()
+    {
+        anim.SetFloat("Speed",inputvector.magnitude);
+        if (inputvector.x != 0)
+        {
+            sprite.flipX = inputvector.x < 0;
+        }
     }
 }

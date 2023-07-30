1498f77d8efa11eaa8033f1116cf6b95b09637ff
bluekigh <bluekigh@gmail.com>
2023-07-30 15:32:57 +0900

    04 map 만들고 재활용 (cinemachine)

---
+++ Assets/Script/TitleMap/Reposition.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Area"))
+        {
+            return;
+        }
+
+        Vector3 playerposition = GameManager.inst.player.transform.position;
+        Vector3 myPos = transform.position;
+
+        float diffx = Mathf.Abs(playerposition.x - myPos.x);
+        float diffy = Mathf.Abs(playerposition.y - myPos.y);
+
+        Vector3 playerDir = GameManager.inst.player.inputvector;
+        float dirX = playerDir.x < 0 ? -1 : 1;
+        float dirY = playerDir.y < 0 ? -1 : 1;
+
+        switch (transform.tag)
+        {
+            case "Ground":
+                if (diffx > diffy)
+                {
+                    transform.Translate(Vector3.right*dirX*40);
+                }
+                else if(diffx < diffy)
+                {
+                    transform.Translate(Vector3.up*dirY*40);
+                }
+                break;
+            case "Enemy":
+                break;
+            default:
+                break;
+        }
+
+    }
+}
